80cce4b7c1738f2ca13e0e583fbd6be9f43474ef
Mustafa Gemsiz <birhatgemsiz@hotmail.com>
2021-09-13 21:04:02 +0300

    first

---
+++ XamarinCalculator/XamarinCalculator/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void temizle(Object sender,EventArgs e)
+        {
+
+        }        
+        void islemsec(Object sender,EventArgs e)
+        {
+
+        }
+
+        void yuzde(Object sender, EventArgs e)
+        {
+
+        }
+
+        void karekok(Object sender, EventArgs e)
+        {
+
+        }
+
+        void sayi(Object sender, EventArgs e)
+        {
+
+        }
+
+        void hesapla(Object sender, EventArgs e)
+        {
+
+        }
+    }
+}
